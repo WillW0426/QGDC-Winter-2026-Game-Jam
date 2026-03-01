c5b8d715ba687759ed1dc350a2ef76a8bc672c42
Nathan Chow <nchow5517@gmail.com>
2026-03-01 01:06:46 -0500

    Added PlayerUI with Reset Button

---
+++ Assets/Scripts/UI/ResetLevelButtonUI.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RestLevelButtonUI : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] Button resetButton;
+
+    [Header("Hover Colors")]
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color hoverColor = Color.yellow;
+
+    public Loader.Scene resetScene;
+
+    private void Awake()
+    {
+        resetButton.onClick.AddListener(() =>
+            Loader.Load(resetScene)
+        );
+
+        AddHoverEffect(resetButton);
+    }
+
+    private void AddHoverEffect(Button button)
+    {
+        TMP_Text buttonText = button.GetComponentInChildren<TMP_Text>();
+
+        if (buttonText == null)
+        {
+            Debug.LogWarning($"{button.name} has no TMP_Text child!");
+            return;
+        }
+
+        // Set initial color
+        buttonText.color = normalColor;
+
+        EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>()
+                            ?? button.gameObject.AddComponent<EventTrigger>();
+
+        // On Hover Enter
+        EventTrigger.Entry enterEntry = new EventTrigger.Entry();
+        enterEntry.eventID = EventTriggerType.PointerEnter;
+        enterEntry.callback.AddListener((_) => buttonText.color = hoverColor);
+        trigger.triggers.Add(enterEntry);
+
+        // On Hover Exit
+        EventTrigger.Entry exitEntry = new EventTrigger.Entry();
+        exitEntry.eventID = EventTriggerType.PointerExit;
+        exitEntry.callback.AddListener((_) => buttonText.color = normalColor);
+        trigger.triggers.Add(exitEntry);
+    }
+}
