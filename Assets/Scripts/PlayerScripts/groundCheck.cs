6fe0d011375c8b7f12329e45b8f0f6e0d9e1d3f5
Tyson Nguyen <nguyentyson03@gmail.com>
2026-03-01 13:23:56 -0800

    Fix

--- Assets/Scripts/PlayerScripts/groundCheck.cs
+++ Assets/Scripts/PlayerScripts/groundCheck.cs
@@ -5,18 +5,19 @@ public class groundCheck : MonoBehaviour
     public bool isGrounded;
     public float downwardRaycastDistance = 1f;
     public LayerMask groundLayerMask;
+    public LayerMask crammableObjects;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        groundLayerMask = LayerMask.GetMask("Ground");
+        crammableObjects = LayerMask.GetMask("Crammable");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Collider2D hit = Physics2D.OverlapCircle(transform.position, 0.1f);
         // Check if the player is grounded using this collider
-        if (hit != null && (hit.gameObject.CompareTag("Ground") || Physics2D.OverlapCircle(transform.position, 0.1f, groundLayerMask)))
+        if (Physics2D.OverlapCircle(transform.position, 0.1f, crammableObjects) || Physics2D.OverlapCircle(transform.position, 0.1f, groundLayerMask))
         {
             isGrounded = true;
         } else
